fa8fa0dc592df87595eaf95613b276695e4f5456
NamaWorks <pedronayamartinez@hotmail.com>
2024-07-19 20:26:16 +0200

    hotfix in vite.config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,12 @@ export default defineConfig({
       input: {
         index: resolve('A37', 'index.html'),
         about: resolve('A37', './src/components/pages/about/about.html'),
-        contact: resolve('A37', './src/components/pages/contact/contact.html')
+        contact: resolve('A37', './src/components/pages/contact/contact.html'),
+        work: resolve('A37', './src/components/pages/work/work.html'),
+        projectOne: resolve('A37', './src/components/pages/projects/project-01/project01.html'),
+        projectTwo: resolve('A37', './src/components/pages/projects/project-02/project02.html'),
+        projectThree: resolve('A37', './src/components/pages/projects/project-03/project03.html'),
+        projectFour: resolve('A37', './src/components/pages/projects/project-04/project04.html'),
       },
     },
   },
