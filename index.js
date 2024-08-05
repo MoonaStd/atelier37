608c1901fdf6d1f36f7b2602997a9d8a3800ed70
NamaWorks <pedronayamartinez@hotmail.com>
2024-08-05 19:32:42 +0200

    quicksave before dependencies changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { printNavBar } from "./src/components/elements/navbar/navbar"
 import { changeCursor} from "./src/utils/changeCursor"
 import { addTypewriterEffect } from "./src/utils/functions/addTypewriterEffect"
 import { printHomeImages } from "./src/utils/functions/loopImagesHome"
+import { scrollToSmoothly } from "./src/utils/functions/smoothScroll"
 
 printNavBar()
 printFooter()
@@ -12,3 +13,8 @@ changeCursor()
 addTypewriterEffect()
 printHomeImages()
 
+window.scrollTo({ behavior: 'smooth'})
+
+document.addEventListener("scroll", (e)=>{
+
+})
